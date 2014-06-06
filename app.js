4caf635c61d9d095f208958228ed9327a6a0f9a8
patmood <patrick.n.moody@gmail.com>
2014-06-05 23:24:01 -0700

    cool comma syntax

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express')
-var path = require('path')
-var favicon = require('static-favicon')
-var logger = require('morgan')
-var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
+  , path = require('path')
+  , favicon = require('static-favicon')
+  , logger = require('morgan')
+  , cookieParser = require('cookie-parser')
+  , bodyParser = require('body-parser')
 
 var routes = require('./routes/index')
-var users = require('./routes/users')
+  , users = require('./routes/users')
 
 var app = express()
 
